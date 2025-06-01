2ac67cc7900b6495887e75a9b9a90e0ad3786fbf
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-06-01 14:31:05 +0600

    Add files via upload

---
+++ Chapter 2 - Introduction to Java Applications; Input, Output and Operators/Welcome3.java
@@ -0,0 +1,19 @@
+public class Welcome3 
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hi\nI am Tusher\nI am interested in Java programming");
+    }
+}
+/*
+Escape sequence         Description
+\n Newline.             Position the screen cursor at the beginning of the next line.
+\t Horizontal tab.      Move the screen cursor to the next tab stop.
+\r Carriage return.     Position the screen cursor at the beginning of the current
+                        line—do not advance to the next line. Any characters output after the car￾
+                        riage return overwrite the characters previously output on that line.
+\\ Backslash.           Used to print a backslash character.
+\" Double quote.        Used to print a double-quote character. For example,
+                        System.out.println("\"in quotes\"");
+                        displays "in quotes".
+ */
