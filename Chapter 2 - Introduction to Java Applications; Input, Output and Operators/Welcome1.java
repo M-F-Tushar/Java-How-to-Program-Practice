38f75319c2f3df0bb27d888b1a897928af51e313
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-06-01 14:30:40 +0600

    Add files via upload

---
+++ Chapter 2 - Introduction to Java Applications; Input, Output and Operators/Welcome1.java
@@ -0,0 +1,11 @@
+public class Welcome1
+{
+    public static void main(String[] args)
+    {
+        //main methos begins execution of java application
+        System.out.println("Welcome to Java Programming!");
+    }//end of main method
+
+}//end of class Welcome1
+/* This is a traditional comment. It
+can be split over multiple lines */
